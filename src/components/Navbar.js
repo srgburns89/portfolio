b308462e6c88ed835223b5793a2bae1921283d39
srgburns89 <72822640+srgburns89@users.noreply.github.com>
2022-01-11 11:46:00 -0800

    navBar complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import logo from "../sb-logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+      <a className="navbar-brand" href="#">
+        <img className="sb-logo" src={logo} alt="SB" />
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <FontAwesomeIcon icon={faBars} style={{ color: "white"}}/>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home 
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Services
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Experience
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      </div>
+
+    </nav>
+  );
+};
+
+export default Navbar;
